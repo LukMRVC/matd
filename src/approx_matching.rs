959d693ce74e56cdd32fa809a84544dedca4ee69
Moravec4, Lukas <lukas.moravec4@firma.seznam.cz>
2023-03-21 16:46:34 +0100

    Approximate matching using NFA

---
+++ src/approx_matching.rs
@@ -0,0 +1,111 @@
+pub fn match_ndfa(text: &str, pattern: &str, max_errors: usize) -> bool {
+    // let mut indices: Vec<usize> = vec![];
+    //             Vec<(state, automaton_ix, pattern_ix, errors)>
+    let pattern_chars: Vec<char> = pattern.chars().collect();
+    let text_chars: Vec<char> = text.chars().collect();
+
+    let mut configs: Vec<(usize, usize, usize, usize)> = vec![(1, 0, 0, 0)];
+
+    // neshoda znaku
+    // - posun o pozici v automatu a posun v pozici query
+    // prazdne slovo
+    // - posun o pozici v automatu, v query zustavam na stejne pozici
+    // symbol navic
+    // - posun v pozici query
+    // shoda znaku
+    // - posun obou pozic
+    let pattern_len = pattern.len();
+    while let Some((state, aix, pix, errors)) = configs.pop() {
+        if state % (pattern_len + 1) == 0 {
+            // indices.push(state);
+            return true;
+        }
+
+        if pix >= text_chars.len() || aix >= pattern_len {
+            continue;
+        }
+
+        if text_chars[pix] == pattern_chars[aix] {
+            configs.push((state + 1, aix + 1, pix + 1, errors));
+        } else if errors < max_errors {
+            // char replace
+            configs.push((state + pattern_len + 2, aix + 1, pix + 1, errors + 1));
+            // empty char - insertion
+            configs.push((state + pattern_len + 2, aix + 1, pix, errors + 1));
+            // char deletion
+            configs.push((state + pattern_len + 1, aix, pix + 1, errors + 1));
+        }
+    }
+
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ndfa_survey_match() {
+        let res = match_ndfa("survey", "murvey", 2);
+        assert_eq!(res, true);
+
+        let res = match_ndfa("survey", "ssurvey", 2);
+        assert_eq!(res, true);
+
+        let res = match_ndfa("survey", "murvxy", 2);
+        assert_eq!(res, true);
+
+        let res = match_ndfa("survey", "murfxy", 2);
+        assert_eq!(res, false);
+    }
+
+    #[test]
+    fn ndfa_test() {
+        let words = vec![
+            "karel",
+            "robot",
+            "kafe",
+            "otoman",
+            "stul",
+            "sysel",
+            "hubert",
+            "kanape",
+            "jurta",
+            "nhl",
+            "ork",
+            "vlak",
+            "zidle",
+            "afrika",
+            "evropa",
+            "slon",
+            "zebra",
+            "saty",
+            "auto",
+            "autobus",
+            "kolo",
+            "fontana",
+            "opera",
+            "rakousko",
+            "hora",
+            "beh",
+            "touha",
+            "kamarad",
+            "pocitac",
+            "procesor",
+            "klavesnice",
+            "mys",
+            "parek",
+        ];
+
+        let pattern = "dale";
+        let mut matches: Vec<&&str> = vec![];
+
+        for w in words.iter() {
+            if match_ndfa(w, pattern, 2) {
+                matches.push(w);
+            }
+        }
+
+        dbg!(matches);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod approx_matching;
 mod string_matching;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -9,19 +10,16 @@ fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
     let pattern = "GCAGC".to_owned();
 
     let stupid_matches = string_matching::match_stupid(&text, &pattern);
-    dbg!(stupid_matches);
 
     // automaton comparison
     let pattern = "GCAGC".to_owned();
 
     let dfa_matches = string_matching::match_dfa(&text, &pattern);
-    dbg!(dfa_matches);
 
     // KMP pattern finding
     let text = "AABAACAADAABAABA".to_owned();
     let pattern = "AABA".to_owned();
     let kmp_matches = string_matching::match_kmp(&text, &pattern);
-    dbg!(kmp_matches);
 
     let dnadatafile = File::open("./data/dna.50MB")?;
     let mut dnareader = BufReader::new(dnadatafile);
@@ -33,9 +31,9 @@ fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
     let mut rng = rand::thread_rng();
     let mut pattern_words: Vec<String> = vec![];
 
-    for _ in 0..1000 {
+    for _ in 0..100 {
         let s = rng.gen_range(0..dnadata.len());
-        let l = rng.gen_range(0..50);
+        let l = rng.gen_range(5..50);
         pattern_words.push(String::from_utf8(Vec::from(&dnadata[s..(s + l)])).unwrap());
     }
 
@@ -43,13 +41,13 @@ fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
         let Ok(text) = String::from_utf8(dnadata.clone()) else {
             panic!("Could not convert DNA data to String");
         };
-        benchmark_pattern_search(
-            &text,
-            &pattern_words,
-            &string_matching::match_stupid,
-            "Stupid",
-        );
-        benchmark_pattern_search(&text, &pattern_words, &string_matching::match_dfa, "DFA");
+        // benchmark_pattern_search(
+        //     &text,
+        //     &pattern_words,
+        //     &string_matching::match_stupid,
+        //     "Stupid",
+        // );
+        // benchmark_pattern_search(&text, &pattern_words, &string_matching::match_dfa, "DFA");
         benchmark_pattern_search(&text, &pattern_words, &string_matching::match_kmp, "KMP");
         benchmark_pattern_search(&text, &pattern_words, &string_matching::match_bmh, "BMH");
     }
@@ -63,9 +61,9 @@ fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
 
     let mut pattern_words: Vec<String> = vec![];
 
-    for _ in 0..1000 {
+    for _ in 0..100 {
         let s = rng.gen_range(0..engdata.len());
-        let l = rng.gen_range(0..10);
+        let l = rng.gen_range(2..10);
         pattern_words.push(String::from_utf8(Vec::from(&engdata[s..(s + l)])).unwrap());
     }
 
@@ -89,12 +87,13 @@ fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
 fn benchmark_pattern_search(
     text: &str,
     patterns: &[String],
-    func: &dyn Fn(&str, &str) -> Vec<usize>,
+    func: &impl Fn(&str, &str) -> Vec<usize>,
     method: &str,
 ) {
     let start = std::time::Instant::now();
-    for pat in patterns.iter() {
+    for (i, pat) in patterns.iter().enumerate() {
         func(&text, pat);
+        println!("{i}")
     }
     println!(
         "{method} matching done in {}ms",
